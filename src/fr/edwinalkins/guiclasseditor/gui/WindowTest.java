36c2cdad7aefd849b07bc9ca34e0021890c4fa12
William NAUROY <william.nauroy@etu.univ-nantes.fr>
2020-10-02 13:40:00 +0200

    [CODE] Add Main test GUI

---
+++ src/fr/edwinalkins/guiclasseditor/gui/WindowTest.java
@@ -0,0 +1,59 @@
+package fr.edwinalkins.guiclasseditor.gui;
+
+import java.awt.EventQueue;
+import java.awt.Panel;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import java.awt.BorderLayout;
+
+public class WindowTest {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+				} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+						| UnsupportedLookAndFeelException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				try {
+					WindowTest window = new WindowTest();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public WindowTest() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Panel panel = new Panel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+	}
+
+}
+
